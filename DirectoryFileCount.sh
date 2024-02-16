0e7ab773cc6402237ab47a53760077b91ca937f6
Mulpurus01 <160132391+Mulpurus01@users.noreply.github.com>
2024-02-16 06:10:46 +0530

    DirectoryFileCount.sh

--- DirectoryFileCount.sh
+++ DirectoryFileCount.sh
@@ -1,1 +1,22 @@
-#
+#!/bin/bash
+#To execute this file permissions are manually set 0740 OR 0640
+#To start the script give sh <script name.sh>
+#To run the script in background and copy the output to file nohup </path/to/filename/filename.sh> &
+echo ----------------------------------------------
+echo 'Started counting the staging file'
+echo ----------------------------------------------
+NUMBER_OF_FILES_STILL_EXIST=0
+
+#LIST OF DIRECTORIES IN WHICH WE NEED TO CHECK
+DIRECTORIES=("LOCATION1" "LOCATION1" "LOCATION2" "LOCATION3" "LOCATION4" "LOCATION5")
+
+for DIRECTORY in "${DIRECTORIES[@]}"
+  do
+    cd "/path/to/folders/$DIRECTORY/any/internal/folder/"
+    echo $(pwd) has : $(ls | wc -l)
+    NUMBER_OF_FILES_STILL_EXIST=$((NUMBER_OF_FILES_STILL_EXIST+$(ls |wc -l)))
+  done
+echo ----------------------------------------------
+echo Todat: $NUMBER_OF_FILES_STILL_EXIST
+echo ----------------------------------------------
+echo HAIL HYDRA
